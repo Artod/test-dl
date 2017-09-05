be4d507b5b546086c85c49d1ad46dfa949220a33
Artod <gartod@gmail.com>
2017-09-04 21:48:04 -0400

    4 day redux, mergeProps...

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import {createStore, combineReducers} from 'redux'
+
 import registerServiceWorker from './registerServiceWorker'
 
 import {store} from './store'
